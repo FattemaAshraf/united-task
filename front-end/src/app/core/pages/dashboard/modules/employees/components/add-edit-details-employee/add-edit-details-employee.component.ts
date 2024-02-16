97566f8256f6a663174285b21dab02f8c34499ae
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-16 22:28:45 +0200

    added add-employee ui form

--- front-end/src/app/core/pages/dashboard/modules/employees/components/add-edit-details-employee/add-edit-details-employee.component.ts
+++ front-end/src/app/core/pages/dashboard/modules/employees/components/add-edit-details-employee/add-edit-details-employee.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-details-employee',
   templateUrl: './add-edit-details-employee.component.html',
-  styleUrls: ['./add-edit-details-employee.component.scss']
+  styleUrls: ['./add-edit-details-employee.component.scss'],
 })
 export class AddEditDetailsEmployeeComponent {
+  departments: string[] = [
+    'General Management',
+    'Marketing Department',
+    'Finance Department',
+    'Sales Department',
+    'Human Resource Department',
+    'Purchase Department',
+  ];
+  constructor() {}
+  addEmployeeForm = new FormGroup({
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('([a-zA-Z]){3,12}'),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    nationalId: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(
+        '(2|3)[0-9][1-9][0-1][1-9][0-3][1-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)ddddd'
+      ),
+    ]),
+    department: new FormControl(null, [Validators.required]),
+    hireDate: new FormControl(null, [Validators.required]),
+    manager: new FormControl(null, [Validators.required]),
+    code: new FormControl(null, [Validators.required]),
 
+  });
 }
--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -3,35 +3,48 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRouteComponent } from './components/not-found-route/not-found-route.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterModule } from '@angular/router';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
-
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  declarations: [
-    NotFoundRouteComponent,
-    NavBarComponent,
-    SideBarComponent
-  ],
+  declarations: [NotFoundRouteComponent, NavBarComponent, SideBarComponent],
   imports: [
     CommonModule,
     MatButtonModule,
     MatIconModule,
     RouterModule,
     MatTooltipModule,
-    RouterLink
-  ],exports:[
+    RouterLink,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
+  ],
+  exports: [
     NotFoundRouteComponent,
     NavBarComponent,
     SideBarComponent,
     MatIconModule,
     RouterModule,
     MatTooltipModule,
-    RouterLink
-
-  ]
+    RouterLink,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
