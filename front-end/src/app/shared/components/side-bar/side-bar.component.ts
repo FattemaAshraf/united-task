5a27d4ca1dde1f69eada3155a51d67093abe8932
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-16 02:00:41 +0200

    added side bar ui

--- front-end/src/app/shared/components/side-bar/side-bar.component.ts
+++ front-end/src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+interface IMenu {
+  title: string;
+  icon: string;
+  link: string;
+}
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+  styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent {
+  @Output() isOpenedValue = new EventEmitter<boolean>();
+  isOpened: boolean = false;
 
+  constructor(
+    private _router: Router,
+  ) {}
+  ngOnInit() {
+      this._router.navigate(['/dashboard/landing-page']);
+  }
+  menu: IMenu[] = [
+    {
+      title: 'Home',
+      icon: 'fa-solid fa-house',
+      link: '/dashboard/landing-page',
+    },
+    {
+      title: 'Employees',
+      icon: 'fa-solid fa-users',
+      link: '/dashboard/employees',
+    },
+  ];
+  onClicked() {
+    this.isOpened = !this.isOpened;
+    this.isOpenedValue.emit(this.isOpened);
+    console.log(this.isOpened);
+  }
 }
--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRouteComponent } from './components/not-found-route/not-found-route.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink, RouterModule } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 
@@ -13,11 +17,21 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
     SideBarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    MatTooltipModule,
+    RouterLink
   ],exports:[
     NotFoundRouteComponent,
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    MatIconModule,
+    RouterModule,
+    MatTooltipModule,
+    RouterLink
+
   ]
 })
 export class SharedModule { }
