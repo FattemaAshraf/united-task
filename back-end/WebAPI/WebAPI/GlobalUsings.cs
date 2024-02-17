bb8b2423680acf80403f2d1d0de8075e70e605ba
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-17 04:16:08 +0200

    added models with first migration

--- back-end/WebAPI/WebAPI/GlobalUsings.cs
+++ back-end/WebAPI/WebAPI/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿global using WebAPI.Models;
 global using Microsoft.EntityFrameworkCore;
 global using WebAPI.Data;
+global using System.ComponentModel.DataAnnotations.Schema;
---
+++ back-end/WebAPI/WebAPI/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
